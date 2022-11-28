1156beb3eda7753ccc539d616ea335b6d3c16e5f
Android Studio <devandroid@ort>
2022-11-28 12:15:45 -0300

    Seguimos peleando con <Clima/>

--- src/Components/Clima.js
+++ src/Components/Clima.js
@@ -1,22 +1,50 @@
 import { React, useContext } from 'react';
 import { View, Text } from 'react-native';
-import geolocation from '@react-native-community/geolocation';
-
-import TextButton from '../Components/TextButton.js';
+import * as Location from 'expo-location';
 
 export default function Clima(){
-    let ubicacion = {};
-    try{
-        geolocation.setRNConfiguration({skipPermissionRequests: false});
-    }catch(e){}
-    try{
-        geolocation.requestAuthorization();
-    }catch(e){}
-    geolocation.getCurrentPosition(
-        () => console.log(location),
-        () => console.warn("No se pudo encontrar la ubicación")
-      );
+    console.log(RNLocation);
+    console.log('antes');
+    let ubicacion = {error: true};
+    await RNLocation.configure({
+        distanceFilter: 5.0
+      })
+      console.log('paso');
+      RNLocation.requestPermission({
+        ios: "whenInUse",
+        android: {
+          detail: "coarse"
+        }
+      }).then(granted => {
+          if (granted) {
+            this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
+                ubicacion = locations;
+              /* Example location returned
+              {
+                speed: -1,
+                longitude: -0.1337,
+                latitude: 51.50998,
+                accuracy: 5,
+                heading: -1,
+                altitude: 0,
+                altitudeAccuracy: -1
+                floor: 0
+                timestamp: 1446007304457.029,
+                fromMockProvider: false
+              }
+              */
+            })
+          }
+        })
     return (
-        <Text>{ubicacion}</Text>
+        <Text>
+            {
+                ubicacion.error
+                ?
+                "No se pudo encontrar la ubicación"
+                :
+                "Altitud: "+ubicacion.altitude+"\nLatitud:"+ubicacion.latitude
+            }
+        </Text>
     );
 }
