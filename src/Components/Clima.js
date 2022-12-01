ca1d4e6429dac90c32570c1ba9593f4aa492aa34
Android Studio <devandroid@ort>
2022-12-01 16:07:47 -0300

    <Clima/> funciona a la perfección

--- src/Components/Clima.js
+++ src/Components/Clima.js
@@ -47,8 +47,17 @@ export default function Clima() {
     <View style={styles.container}>
       <View style={styles.table}>
         <View style={styles.row}>
+          <Text style={styles.cell}>Lugar</Text>
+          <Text style={styles.cell}>{clima.name}</Text>
+        </View>
+        <View style={styles.row}>
+        
           <Text style={styles.cell}>Temperatura</Text>
           <Text style={styles.cell}>{clima.main.temp}°</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.cell}>Sensación térmica</Text>
+          <Text style={styles.cell}>{clima.main.feels_like}°</Text>
         </View>
         <View style={styles.row}>
           <Text style={styles.cell}>Temperatura máxima</Text>
@@ -59,33 +68,19 @@ export default function Clima() {
           <Text style={styles.cell}>{clima.main.temp_min}°</Text>
         </View>
         <View style={styles.row}>
-          <Text style={styles.cell}>Temperatura</Text>
-          <Text style={styles.cell}>{clima.main.temp}</Text>
+          <Text style={styles.cell}>Humedad</Text>
+          <Text style={styles.cell}>{clima.main.humidity}%</Text>
         </View>
         <View style={styles.row}>
-          <Text style={styles.cell}>Temperatura</Text>
-          <Text style={styles.cell}>{clima.main.temp}</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.cell}>Temperatura</Text>
-          <Text style={styles.cell}>{clima.main.temp}</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.cell}>Temperatura</Text>
-          <Text style={styles.cell}>{clima.main.temp}</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.cell}>Temperatura</Text>
-          <Text style={styles.cell}>{clima.main.temp}</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.cell}>Temperatura</Text>
-          <Text style={styles.cell}>{clima.main.temp}</Text>
+          <Text style={styles.cell}>Viento</Text>
+          <Text style={styles.cell}>{clima.wind.deg}° a {clima.wind.speed}km/h</Text>
         </View>
       </View>
-      <SvgButton style={styles.mapButton} viewBox="-2 -2 20 20" onClick={getClima}>
-        M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973z
-      </SvgButton>
+      <View style={styles.centerButton}>
+        <SvgButton style={styles.mapButton} viewBox="-2 -2 20 20" onClick={getClima}>
+          M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973z
+        </SvgButton>
+      </View>
     </View>
   );
 
@@ -102,6 +97,7 @@ const styles = StyleSheet.create({
   },
   cell: {
     border: "solid 0.5px",
+    width: "50%",
     padding: "3px"
   },
   mapButton: {
